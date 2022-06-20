3e4dce9eeb7f6da3227761f32c690fd4d89ae0ec
LucasiBai <lucasibaigorria@mi.unc.edu.ar>
2022-06-20 12:04:17 -0300

    fix:afinando tiempo de carga + width de cards + no cotiza sin $

--- js/actulizarPrecioDolar.js
+++ js/actulizarPrecioDolar.js
@@ -2,35 +2,37 @@ const URLDOLAR = "https://www.dolarsi.com/api/api.php?type=valoresprincipales";
 const tablaPrecios = document.querySelector("#tablaPrecioDolares");
 const fecha = new Date();
 const dia =
-  String(fecha.getDate()).padStart(2, "0") +
-  "/" +
-  String(fecha.getMonth() + 1).padStart(2, "0") +
-  "/" +
-  String(fecha.getFullYear() + 1).padStart(2, "0");
+	String(fecha.getDate()).padStart(2, "0") +
+	"/" +
+	String(fecha.getMonth() + 1).padStart(2, "0") +
+	"/" +
+	String(fecha.getFullYear() + 1).padStart(2, "0");
 const horas = String(fecha.getHours());
 const minutos = String(fecha.getMinutes());
 const segundos = String(fecha.getSeconds());
 
 function traerPreciosDolar() {
-  fetch(URLDOLAR)
-    .then((res) => res.json())
-    .then((data) => {
-      let preciosDolar = data;
-      preciosDolar
-        .filter(
-          (precio) =>
-            precio.casa.nombre !== "Argentina" &&
-            precio.casa.venta != 0 &&
-            precio.casa.variacion
-        )
-        .forEach((precio) => {
-          tablaPrecios.innerHTML += `
+	fetch(URLDOLAR)
+		.then((res) => res.json())
+		.then((data) => {
+			let preciosDolar = data;
+			preciosDolar
+				.filter(
+					(precio) =>
+						precio.casa.nombre !== "Argentina" &&
+						precio.casa.venta != 0 &&
+						precio.casa.variacion
+				)
+				.forEach((precio) => {
+					tablaPrecios.innerHTML += `
         <section class="cuenta-info">      
         <h1 class="tipo__dolar">${precio.casa.nombre}</h1>
         <section class="dolar__precio">
           <article class="dolar__precio--articulo">
             <h4>Compra</h4>
-            <h2>$${precio.casa.compra}</h2>
+            <h2>${precio.casa.compra != "No Cotiza" ? "$" : ""}${
+						precio.casa.compra
+					}</h2>
           </article>
           <article class="dolar__precio--articulo">
             <h4>Venta</h4>
@@ -40,11 +42,11 @@ function traerPreciosDolar() {
         <div> 
         <article class="dolar__precio--datos">
         <div class= "${
-          precio.casa.variacion.indexOf("-") < 0 ? conoverde : conorojo
-        }">    </div>
+					precio.casa.variacion.indexOf("-") < 0 ? conoverde : conorojo
+				}">    </div>
           <h4 class="dolar__precio--dato">VARIACION ${
-            precio.casa.variacion
-          }%</h4>
+						precio.casa.variacion
+					}%</h4>
         <h5 class="dolar__precio--dato center">ACTUALIZADO: <br> ${dia} ${horas}:${minutos}:${segundos}</h5>
 
           
@@ -52,16 +54,17 @@ function traerPreciosDolar() {
         </article>
       </section>
         `;
-        });
-    });
+				});
+		});
 }
 
-window.addEventListener("load", function () {
-  this.document.getElementById("loader").classList.toggle("loader2");
-});
+function esconderLoader() {
+	this.document.getElementById("loader").classList.toggle("loader2");
+}
 
 window.onload = () => {
-  traerPreciosDolar();
+	traerPreciosDolar();
+	setTimeout(esconderLoader, 320);
 };
 let conorojo = "cono-rojo";
 let conoverde = "cono-verde";
