adee087da382a9c94301c1f90e86f382f86fd6c2
Tomas <tomasguerra308@outlook.com>
2022-06-19 22:20:19 -0300

    Flechas fluctuacion dolar V1.0

--- js/actulizarPrecioDolar.js
+++ js/actulizarPrecioDolar.js
@@ -1,30 +1,30 @@
-
 const URLDOLAR = "https://www.dolarsi.com/api/api.php?type=valoresprincipales";
 const tablaPrecios = document.querySelector("#tablaPrecioDolares");
 const fecha = new Date();
-const dia =  String(fecha.getDate()).padStart(2, '0') + '/' + String(fecha.getMonth() + 1).padStart(2, '0')+ '/' + String(fecha.getFullYear() + 1).padStart(2, '0');
+const dia =
+  String(fecha.getDate()).padStart(2, "0") +
+  "/" +
+  String(fecha.getMonth() + 1).padStart(2, "0") +
+  "/" +
+  String(fecha.getFullYear() + 1).padStart(2, "0");
 const horas = String(fecha.getHours());
 const minutos = String(fecha.getMinutes());
 const segundos = String(fecha.getSeconds());
 
-
-
-
 function traerPreciosDolar() {
-	fetch(URLDOLAR)
-		.then((res) => res.json())
-		.then((data) => {
-			let preciosDolar = data;
-			preciosDolar
-				.filter(
-					(precio) =>
-						precio.casa.nombre !== "Argentina" &&
-						precio.casa.venta != 0 &&
-						precio.casa.variacion
-				)
-				.forEach((precio) => {
-          
-					tablaPrecios.innerHTML += `
+  fetch(URLDOLAR)
+    .then((res) => res.json())
+    .then((data) => {
+      let preciosDolar = data;
+      preciosDolar
+        .filter(
+          (precio) =>
+            precio.casa.nombre !== "Argentina" &&
+            precio.casa.venta != 0 &&
+            precio.casa.variacion
+        )
+        .forEach((precio) => {
+          tablaPrecios.innerHTML += `
         <section class="cuenta-info">      
         <h1 class="tipo__dolar">${precio.casa.nombre}</h1>
         <section class="dolar__precio">
@@ -37,24 +37,31 @@ function traerPreciosDolar() {
             <h2>$${precio.casa.venta}</h2>
           </article>
         </section>
+        <div> 
         <article class="dolar__precio--datos">
-
-          <h4 class="dolar__precio--dato">VARIACION ${precio.casa.variacion}%</h4>
-        <h5 class="dolar__precio--dato ">ACTUALIZADO: <br> ${dia} ${horas}:${minutos}:${segundos}</h5>
+        <div class= "${
+          precio.casa.variacion.indexOf("-") < 0 ? conoverde : conorojo
+        }">    </div>
+          <h4 class="dolar__precio--dato">VARIACION ${
+            precio.casa.variacion
+          }%</h4>
+        <h5 class="dolar__precio--dato center">ACTUALIZADO: <br> ${dia} ${horas}:${minutos}:${segundos}</h5>
 
           
 
         </article>
       </section>
         `;
-				});
-		});
+        });
+    });
 }
 
-window.addEventListener("load", function(){
-  this.document.getElementById("loader").classList.toggle("loader2")
-})
+window.addEventListener("load", function () {
+  this.document.getElementById("loader").classList.toggle("loader2");
+});
 
 window.onload = () => {
-	traerPreciosDolar();
+  traerPreciosDolar();
 };
+let conorojo = "cono-rojo";
+let conoverde = "cono-verde";
